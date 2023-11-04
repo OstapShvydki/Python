3c118078bb9bdf1c9a6be1e08b6d90ce2c046f62
OstapShvydki <surpohit@gmail.com>
2023-11-04 18:07:40 +0000

    Data types, print, input, variable naming conventions

---
+++ basic.py
@@ -0,0 +1,27 @@
+# print ("Ostap")
+# print function is used to display the content in the terminal.
+# symbol is used to make a comment. Comment is ignored by python and its for us.
+# name = input ("enter your name:")
+# print (name)
+# input function takes input user from the terminal
+
+# Data Types in Python
+# 1. int=92,354,47
+# 2. float=Decimal point number = 3.5 3.9 
+# 3. string=word, sentence, paragraph="Ostap", 'I am apple', '''Hello World''', """apple"""
+# 4. char="a", "T", "&"
+# 5. bool=True, False
+
+# Variable naming convention
+# 1. Lowercase and upper case alphabets are possible in a variable name.
+# naMe="Ostap"
+# print(naMe)
+# 2. Numbers are possible everywhere exept at the start. 
+# 1na1me12 = "Ostap"
+# print(1na1me12)
+# 3. Symbols are not possible anywhere except "_" symbol.
+# _na_me_=9
+# print(_na_me_)
+# 4. Spaces are not possible anywhere. 
+# na me=9
+# print(na me)
