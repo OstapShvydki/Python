c9e6b98eeb10391ed145640b5b1812b1919b2af7
OstapShvydki <surpohit@gmail.com>
2023-12-02 10:00:00 +0000

    string function

--- string.py
+++ string.py
@@ -20,10 +20,29 @@ mystr = "Leonardo and Ostap are learning python"
 # print(mystr[-6:])
 #original_string = "Python is a powerful programming language."
 #print(original_string[3,6,9])
-mystr = "Python is a powerful programming language."
+mystr = "Python is a powerful programminng language."
 #print(mystr[-9:-1:1])
 # print(mystr[33:41:1])
 # print(mystr[-1:-41:-1])
 # reversed_string = mystr[-1:-41:-1]
 # print(reversed_string[5:13:1])
-print(mystr[3:10:2])
+# print(mystr[3:10:2])
+# string function
+# print(mystr.lower())
+# print(mystr.upper())
+# print(mystr.capitalize())
+# print(mystr.count("a"))
+#count function counts the accurance of a particular characters of a word in a string.
+# print(mystr.count("z"))
+# print(mystr.find("z"))
+#find function gives you the index of the charecter which you are searching for and if you don't get it it gives you -1
+# print(mytsr.index("i")) 
+#index function is the same as find but if there is no charecter like that it gives you an error
+# print(mystr.endswith("."))
+#the endswith function gives you a true or false signal if a word or 
+# print(mystr.startswith("Python"))
+# mystr="4547583748957"
+# print(mystr.isnumeric())
+#the isnumeric function checks if the mystr has all charecters say any number 
+# print(mystr.isalnum())
+#the isalnum function tells you if your string only has letters and numbers it 
