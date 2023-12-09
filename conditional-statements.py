0854e2b61540e6b73a0c129c9f338ff308c5451f
OstapShvydki <surpohit@gmail.com>
2023-12-09 10:05:36 +0000

    conditioned st

---
+++ conditional-statements.py
@@ -0,0 +1,9 @@
+'''
+mood=input("do you want to eat an ice cream? ")
+mood=mood.lower()
+if mood=="yes":
+    print("eat an ice cream🍦")
+else:
+    print("I am not in the mood🥶")  
+ ''' 
+print("What do you want to eat. Here are the options \n1: Burger\n2: Pizza\n3: Donut\n4: French Fries\n5: cookies\n6: Taco\n7: Salad\n8: cake\n9: sushi\n10: popcorn\n11: pancakes ")
