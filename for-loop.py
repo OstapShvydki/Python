e7d06e51f5324ceb64b37625544ad24501c40776
OstapShvydki <surpohit@gmail.com>
2024-02-24 09:56:13 +0000

    for loops and range

---
+++ for-loop.py
@@ -0,0 +1,11 @@
+#for i in range(1, 10):
+#    print(i*"🐷Pig🐷")
+
+#for i in range(1, 10):
+#    if i % 2 == 0: # even rows
+#        print(i*"🐽🐽")
+#    else:
+#        print(i*"🐷🐷")
+
+for i in range(10, 0, -2):
+    print(i*"🐖🐖")
