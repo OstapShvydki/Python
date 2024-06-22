2d257c984a76e1962ef5f0fdbd873c032e191370
OstapShvydki <surpohit@gmail.com>
2024-06-22 10:07:06 +0100

    operators

--- operators.py
+++ operators.py
@@ -9,3 +9,38 @@ print(2 % 3)
 print(2 // 3)
 print(2 ** 3)
 
+#assignment operator 
+x = 5
+print(x)
+x += 1
+print(x)
+x -= 2
+print(x)
+x *= 3
+print(x)
+x /= 3
+print(x)
+x //= 3
+print(x)
+x **= 2
+print(x)
+x %= 3
+print(x)
+
+#Relational operators
+print(4>5)
+print(4>=5)
+print(4<5)
+print(4<=5)
+print(4==5)
+print(4!=5)
+
+#Logical operators
+print((4>5) and (5<4))
+print((4==5) or (5<4))
+print((4<5) and (3<4))
+print((4==4) or (3>4))
+print(not(4<5))
+print(not(6>5))
+print(not(4>5))
+print(not(4 > 3) and 4 < 5)
--- questions.py
+++ questions.py
@@ -44,16 +44,75 @@
 
 #you need to take two strings as input and check which one has more length
 
-x=(input("enter a string(x):     "))
-y=(input("enter a string(y):     "))
-if len(x) > len(y):
-    print("x is longer")
-elif len(x) < len(y):
-    print("y is longer")
-else:
-    print("they are the same length")
+#x=(input("enter a string(x):     "))
+#y=(input("enter a string(y):     "))
+#if len(x) > len(y):
+#    print("x is longer")
+#elif len(x) < len(y):
+#    print("y is longer")
+#else:
+#    print("they are the same length")
 
 #you need to take two strings and check if their first character matches or not. Print yes or no accordingly
 
-x=("pig")
-y=("piglet")
+#x=("cow")
+#y=("sheep")
+#if x[0] == y[0]:
+#    print("yes")
+#else:
+#    print("no")
+
+#Write a Python program to determine the classification of a movie based on its release year and critical rating. 
+#Use year_released for the year it came out and rating for its score out of 10. 
+#Assume the current year is 2024. Classify the movie as follows:
+#If released within the last 5 years:
+#Over 8.0 rating: "Modern hit"
+#5.0 to 8.0 rating: "Modern average"
+#Below 5.0 rating: "Modern flop"
+#If released 5 to 20 years ago:
+#Over 8.0 rating: "Recent classic"
+#5.0 to 8.0 rating: "Recent average"
+#Below 5.0 rating: "Recent miss"
+#If released more than 20 years ago:
+#Over 8.0 rating: "Certified classic"
+#5.0 to 8.0 rating: "Old but good"
+#Below 5.0 rating: "Old flop"
+
+#name = "Harry_Potter"
+#year = 0-2003
+#rating = 7
+
+#if year == 2019-2024 and rating > 8:
+#    print("Modern hit")
+# elif year == 2019-2024 and rating < 8 and rating > 5:
+#     print("Modern average")
+# elif year == 2019-2024 and rating < 5:
+#     print("Modern flop")
+# elif year == 2004-2018 and rating > 8:
+#     print("Recent classic")
+# elif year == 2004-2018 and rating < 8 and rating > 5:
+#     print("Recent average")
+# elif year == 2004-2018 and rating < 5:
+#     print("Recent Miss")
+# elif year == 0-2003 and rating > 8:
+#     print("Certified classic")
+# elif year == 0-2003 and rating < 8 and rating > 5:
+#     print("Old but good")
+# elif year == 0-2003 and rating < 5:
+#     print("Old flop")
+
+# Print all the numbers from 1 to 50 which are divisible by 3 and 5
+
+# for i in range (1, 50):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print(i)
+
+# print the square numbers from 1 - 10
+
+# for i in range (1, 11):
+#         print(i * i)
+
+#Print all of the even numbers from 1 to 20
+
+for i in range (1, 20):
+    print(i % 2 == 0)
